00677f342dfb170be7cb490fe6cf7a9c7c755c73
Noiana Lagras <noi-lagras@hotmail.com>
2023-11-08 17:25:00 -0300

    correccion

--- src/public/js/swal.js
+++ src/public/js/swal.js
@@ -1,18 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const mensajeMostrado = sessionStorage.getItem("mensajeBienvenidaMostrado");
+
+  if (!mensajeMostrado) {
     const btn = document.querySelector(".btn-success");
     const nombreUsuario = btn ? btn.textContent.trim().replace("👤", "") : "";
     const esAdmin = btn ? nombreUsuario.includes("Administrador") : false;
-  
+
     let mensajeBienvenida = `Bienvenido, ${nombreUsuario}`;
     if (esAdmin) {
       mensajeBienvenida += " (Administrador)";
     } else {
       mensajeBienvenida += " (Usuario)";
     }
-  
+
     Swal.fire({
       icon: "success",
       title: "¡Bienvenido!",
       text: mensajeBienvenida,
     });
-  });
+
+     sessionStorage.setItem("mensajeBienvenidaMostrado", "true");
+  }
+});
