2fd4975d1bc6ae3715c438ae26b465576426e424
Noiana Lagras <noi-lagras@hotmail.com>
2024-03-12 14:38:58 -0300

    corrigiendo

--- src/public/js/multerForm.js
+++ src/public/js/multerForm.js
@@ -1,26 +1,4 @@
-/* 
-async function submitForm() {
-    const formData = new FormData(document.getElementById('productForm'));
 
-    try {
-        const response = await fetch('/products/multer', {
-            method: 'POST',
-            body: formData,
-        });
-
-        if (!response.ok) {
-            const errorData = await response.json();
-            throw new Error(errorData.message || 'Error al crear el producto');
-        }
-
-        const result = await response.json();
-        alert(result.message);
-    } catch (error) {
-        console.error('Error al enviar el formulario:', error);
-        alert('error en solicitud'+error)
-    }
-} */
-const socketClient = io();
 document.addEventListener('DOMContentLoaded', () => {
     const multerSubmitBtn = document.getElementById('multerSubmitBtn');
     const productForm = document.getElementById('productForm');
